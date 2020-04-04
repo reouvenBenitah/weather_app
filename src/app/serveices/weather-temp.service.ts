135f00d10ef1b647e8ba3b0253e78d985accb5e8
Reuven Benitah <47301878+reouvenBenitah@users.noreply.github.com>
2020-04-04 21:57:11 +0300

    add parameter

--- src/app/serveices/weather-temp.service.ts
+++ src/app/serveices/weather-temp.service.ts
@@ -10,14 +10,14 @@ export class WeatherTempService {
 
 //if not pass argument so the was location tel aviv 
   getWeather(location="tel%20aviv"){
-    return this.http.get("http://api.weatherstack.com/current?access_key=6e642065ef77747a963102c86a912900&query="+location);
+    return this.http.get("http://api.weatherstack.com/current?access_key=KEY_API&query="+location);
   
   }
 
 
   // getWeatherWeek(locationKey=215854){
 
-  //   return this.http.get('http://dataservice.accuweather.com/forecasts/v1/daily/5day/locationKey?apikey=Gi3een1ldFiVGxImtGlGbHabpu1EJsuh&language=en')
+  //   return this.http.get('http://dataservice.accuweather.com/forecasts/v1/daily/5day/locationKey?apikey=KEY_API&language=en')
   // }
 }
 
